d9025984684c8260d81947f0bdeef489c26fed3f
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-03-21 15:09:01 +0200

    added more styling and markup

---
+++ src/app/home/home-view3/home-view3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-view3',
+  templateUrl: './home-view3.component.html',
+  styleUrls: ['./home-view3.component.css']
+})
+export class HomeView3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { TopViewComponent } from './top-view/top-view.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { HomeView2Component } from './home-view2/home-view2.component';
+import { HomeView3Component } from './home-view3/home-view3.component';
 
 
 @NgModule({
-  declarations: [TopViewComponent, HomeView2Component],
+  declarations: [TopViewComponent, HomeView2Component, HomeView3Component],
   imports: [
     CommonModule,
     HomeRoutingModule,
