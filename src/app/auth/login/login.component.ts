8469dfcf48281d487b265176993c301aa93efe20
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-03-24 11:03:48 +0200

    fixed top page responsivness

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,8 +7,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  hide = true;
+  hide: boolean = true;
   emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.pattern(this.emailRegex)]),
     password: new FormControl('', [Validators.required])
@@ -21,6 +22,7 @@ export class LoginComponent implements OnInit {
 
   onLoginClick(){
     console.log(`form`)
+    // TO DO !!!!!!!!!
   }
 
 }
