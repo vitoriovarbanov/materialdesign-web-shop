5f8316bb981724a1a4fa47c1819a1c3c4fda5a00
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-03-24 12:52:21 +0200

    added window event for opening and closing menu

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-
+  showClose = false;
+  @HostListener('window:click', ['$event.target'])
+  onClick(e){
+    if(e.textContent === 'menu'){
+      this.showClose = true
+    }else{
+      this.showClose = false
+    }
+    console.log(e.textContent)
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleClose(){
+    /* if(!this.showClose){
+      this.showClose = true
+    }else{
+      this.showClose = false
+    }
+    console.log(this.showClose) */
+  }
 }
