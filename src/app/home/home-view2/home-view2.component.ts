4616ca4ed19f712cf48ea03b640c30c0000793f9
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-03-24 15:19:30 +0200

    updated markup and styling

--- src/app/home/home-view2/home-view2.component.ts
+++ src/app/home/home-view2/home-view2.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeView2Component implements OnInit {
   displayedViews = [
-    { src: '../../../assets/health1.jpg', text: 'Lorem Ipsum Text Nice'},
-    { src: '../../../assets/health2.jpg', text: 'Lorem Ipsum Text Nice'},
-    { src: '../../../assets/health3.jpg', text: 'Lorem Ipsum Text Nice'},
-    { src: '../../../assets/health4.jpg', text: 'Lorem Ipsum Text Nice'},
-    { src: '../../../assets/health5.jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health1 (1).jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health2 (1).jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health3 (1).jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health4 (1).jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health5 (1).jpg', text: 'Lorem Ipsum Text Nice'},
   ]
 
   constructor() { }
