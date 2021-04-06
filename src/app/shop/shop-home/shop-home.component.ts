292506dfa24fe27733ed17db48a6c52e4b355d91
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-04-06 17:18:13 +0300

    added new images and improved styling on landing shop page

--- src/app/shop/shop-home/shop-home.component.ts
+++ src/app/shop/shop-home/shop-home.component.ts
@@ -13,12 +13,12 @@ export class ShopHomeComponent implements OnInit {
   }
 
   images = [
-    { src: "../../../assets/shop-product-categories/1.jpg", alt: 'display-alt-text-here'},
-    { src: "../../../assets/shop-product-categories/2.jpg", alt: 'display-alt-text-here'},
+    { src: "../../../assets/shop-product-categories/8.jpg", alt: 'display-alt-text-here', text: 'Food and Beverages'},
+    { src: "../../../assets/shop-product-categories/9.jpg", alt: 'display-alt-text-here', text: 'Herbs'},
     { src: "../../../assets/shop-product-categories/3.jpg", alt: 'display-alt-text-here'},
     { src: "../../../assets/shop-product-categories/4.jpg", alt: 'display-alt-text-here'},
     { src: "../../../assets/shop-product-categories/5.jpg", alt: 'display-alt-text-here'},
-    { src: "../../../assets/shop-product-categories/6.jpg", alt: 'display-alt-text-here'},
+    { src: "../../../assets/shop-product-categories/2.jpg", alt: 'display-alt-text-here'},
     { src: "../../../assets/shop-product-categories/7.jpg", alt: 'display-alt-text-here'},
   ]
 
