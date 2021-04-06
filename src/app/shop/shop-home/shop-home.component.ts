d6c804c52694dadea2225825a65ed304d4d79965
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-04-06 17:53:00 +0300

    added icon for shopping cart

--- src/app/shop/shop-home/shop-home.component.ts
+++ src/app/shop/shop-home/shop-home.component.ts
@@ -14,12 +14,12 @@ export class ShopHomeComponent implements OnInit {
 
   images = [
     { src: "../../../assets/shop-product-categories/8.jpg", alt: 'display-alt-text-here', text: 'Food and Beverages'},
-    { src: "../../../assets/shop-product-categories/9.jpg", alt: 'display-alt-text-here', text: 'Herbs'},
-    { src: "../../../assets/shop-product-categories/3.jpg", alt: 'display-alt-text-here'},
-    { src: "../../../assets/shop-product-categories/4.jpg", alt: 'display-alt-text-here'},
-    { src: "../../../assets/shop-product-categories/5.jpg", alt: 'display-alt-text-here'},
-    { src: "../../../assets/shop-product-categories/2.jpg", alt: 'display-alt-text-here'},
-    { src: "../../../assets/shop-product-categories/7.jpg", alt: 'display-alt-text-here'},
+    { src: "../../../assets/shop-product-categories/9.jpg", alt: 'display-alt-text-here', text: 'Herbs and Botanicals'},
+    { src: "../../../assets/shop-product-categories/10.jpg", alt: 'display-alt-text-here', text: 'Beauty and Personal care'},
+    { src: "../../../assets/shop-product-categories/5.jpg", alt: 'display-alt-text-here', text: 'Sports and Fitness'},
+    { src: "../../../assets/shop-product-categories/11.jpg", alt: 'display-alt-text-here', text: 'Vitamins and Supplements'},
+    { src: "../../../assets/shop-product-categories/12.jpg", alt: 'display-alt-text-here', text: 'Superfoods'},
+    { src: "../../../assets/shop-product-categories/13.jpg", alt: 'display-alt-text-here', text: 'Diet'},
   ]
 
 }
