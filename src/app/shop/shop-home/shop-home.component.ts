0d4b73903b8a4828afc9dc9014f8a0e7ee7a3c7a
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-04-06 21:19:44 +0300

    added image slideshow with slider

--- src/app/shop/shop-home/shop-home.component.ts
+++ src/app/shop/shop-home/shop-home.component.ts
@@ -6,20 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-home.component.css']
 })
 export class ShopHomeComponent implements OnInit {
-
+  Imagedata = [
+    '../../../assets/shop-home/header-top/1.jpg',
+    '../../../assets/shop-home/header-top/2.jpg',
+    '../../../assets/shop-home/header-top/3.jpg'
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    this.Repeat()
   }
 
   images = [
-    { src: "../../../assets/shop-product-categories/8.jpg", alt: 'display-alt-text-here', text: 'Food and Beverages'},
-    { src: "../../../assets/shop-product-categories/9.jpg", alt: 'display-alt-text-here', text: 'Herbs and Botanicals'},
-    { src: "../../../assets/shop-product-categories/10.jpg", alt: 'display-alt-text-here', text: 'Beauty and Personal care'},
-    { src: "../../../assets/shop-product-categories/5.jpg", alt: 'display-alt-text-here', text: 'Sports and Fitness'},
-    { src: "../../../assets/shop-product-categories/11.jpg", alt: 'display-alt-text-here', text: 'Vitamins and Supplements'},
-    { src: "../../../assets/shop-product-categories/12.jpg", alt: 'display-alt-text-here', text: 'Superfoods'},
-    { src: "../../../assets/shop-product-categories/13.jpg", alt: 'display-alt-text-here', text: 'Diet'},
+    { src: "../../../assets/shop-product-categories/8.jpg", alt: 'display-alt-text-here', text: 'Food and Beverages' },
+    { src: "../../../assets/shop-product-categories/9.jpg", alt: 'display-alt-text-here', text: 'Herbs and Botanicals' },
+    { src: "../../../assets/shop-product-categories/10.jpg", alt: 'display-alt-text-here', text: 'Beauty and Personal care' },
+    { src: "../../../assets/shop-product-categories/5.jpg", alt: 'display-alt-text-here', text: 'Sports and Fitness' },
+    { src: "../../../assets/shop-product-categories/11.jpg", alt: 'display-alt-text-here', text: 'Vitamins and Supplements' },
+    { src: "../../../assets/shop-product-categories/12.jpg", alt: 'display-alt-text-here', text: 'Superfoods' },
+    { src: "../../../assets/shop-product-categories/13.jpg", alt: 'display-alt-text-here', text: 'Diet' },
   ]
+
+  Repeat() {
+    setTimeout(() => {
+      this.__FunctionSlide();
+      this.Repeat();
+    }, 2000);
+  }
+  startIndex = 0;
+
+  __FunctionSlide() {
+    const slides = Array.from(document.getElementsByClassName('mall-show-slide'));
+    if (slides === []) {
+      this.Repeat();
+    }
+    for (const x of slides) {
+      const y = x as HTMLElement;
+      y.style.display = 'none';
+    }
+    if (this.startIndex > slides.length - 1) {
+      this.startIndex = 0;
+      const slide = slides[this.startIndex] as HTMLElement;
+      slide.style.display = 'block';
+      this.startIndex++;
+    } else {
+
+      const slide = slides[this.startIndex] as HTMLElement;
+      slide.style.display = 'block';
+      this.startIndex++;
+    }
+  }
 
 }
