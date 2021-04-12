a18f3417a498b3d94efe6bc2313f1bf1bf91c541
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-04-12 15:05:18 +0300

    added routes & component to display each item's details page

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -4,11 +4,14 @@ import { ShopHomeComponent } from './shop-home/shop-home.component';
 import { FoodBeveragesComponent } from './food-beverages/food-beverages.component';
 import { SportsComponent } from './sports/sports.component';
 import { DisplayCartItemsComponent } from './display-cart-items/display-cart-items.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', component: ShopHomeComponent },
   { path: 'foodbvg', component: FoodBeveragesComponent },
+  { path: 'foodbvg/:id', component: ProductDetailsComponent },
   { path: 'sports', component: SportsComponent },
+  { path: 'sports/:id', component: ProductDetailsComponent },
   { path: 'cart', component: DisplayCartItemsComponent}
 ];
 
