23e96d8273f47beee36183ac36332e43dff90b25
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-04-15 10:19:00 +0300

    added interfaces to reviews service

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ReviewsService } from './reviews.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-details',
@@ -11,24 +16,54 @@ import { ReviewsService } from './reviews.service';
 export class ProductDetailsComponent implements OnInit {
   productDetails
   imgCategory = this.router.snapshot.url[0].path
+  rating
 
   reviewsForm = new FormGroup({
     comment: new FormControl('', [Validators.required]),
   })
-  constructor(private router: ActivatedRoute,private reviewsSrvc: ReviewsService) { }
+  constructor(private router: ActivatedRoute, private reviewsSrvc: ReviewsService,
+    private _snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.productDetails = this.router.snapshot.data
-    console.log(this.productDetails)
+    this.reviewsSrvc.getItemRating(this.imgCategory, this.router.snapshot.url[1].path)
+      .subscribe(data=>console.log(data))
+    /* console.log(this.productDetails)
+    const allr = Array.from(document.getElementsByClassName('rating__control'))
+    for (const el of allr) {
+      if (el.id === this.rating) {
+        el.setAttribute("checked", '');
+      } else {
+      }
+    } */
   }
 
-  postProductReview(){
-    this.reviewsSrvc.createPostReviewRequest(this.imgCategory,this.router.snapshot.url[1].path,this.reviewsForm.value.comment,4)
+  postProductReview() {
+    this.reviewsSrvc.createPostReviewRequest(this.imgCategory, this.router.snapshot.url[1].path, this.reviewsForm.value.comment, 4)
   }
 
-  clickedHere(e){
-    e.target.setAttribute("checked", "checked");
-    console.log(e.target)
+  clickedHere(e) {
+    const allr = Array.from(document.getElementsByClassName('rating__control'))
+    for (const el of allr) {
+      if (el.id === e.target.id) {
+        el.setAttribute("checked", '');
+      } else {
+        el.setAttribute("checked", "false")
+      }
+    }
+    this.openSnackBar()
+    /* e.target.setAttribute("checked", "checked");
+    console.log(e.target) */
+  }
 
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  openSnackBar() {
+    this._snackBar.open('Cannonball!!', 'End now', {
+      duration: 2000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
   }
 }
--- src/app/shop/product-details/reviews.service.ts
+++ src/app/shop/product-details/reviews.service.ts
@@ -4,6 +4,34 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import firebase from 'firebase/app'
 import { map } from 'rxjs/operators';
 
+interface AllProductsInList {
+  documents: {
+    fields: {
+      index:{
+        integerValue: string,
+      }
+    }
+  }[]
+}[]
+
+interface ItemRating {
+  reviews: {
+    arrayValue: {
+      values: {}[]
+    }
+  }
+}
+
+interface AllRatings{
+  mapValue: {
+    fields: {
+      rating: {
+        integerValue: string;
+      }
+    }
+  }
+}[]
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,22 +39,35 @@ export class ReviewsService {
 
   constructor(private http: HttpClient, private firestoreDatabase: AngularFirestore) { }
 
-  createPostReviewRequest(category,id,reviewText,rating){
+  createPostReviewRequest(category, id, reviewText, rating) {
     const firestore = firebase.firestore();
     const col = firestore.collection(category)
-    const query = col.where('index','==',Number(id));
+    const query = col.where('index', '==', Number(id));
     query.get()
-    .then((querySnapshot) => {
+      .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
           var docRef = this.firestoreDatabase.collection(category).doc(doc.id);
           docRef.update({
-            reviews: firebase.firestore.FieldValue.arrayUnion({text: reviewText, rating})
+            reviews: firebase.firestore.FieldValue.arrayUnion({ text: reviewText, rating })
+          });
+          console.log(doc.id, " => ", doc.data());
         });
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log("Error getting documents: ", error);
-    });
+      });
+  }
+
+  getItemRating(category, productID) {
+    return this.http.get(`https://firestore.googleapis.com/v1/projects/health-web-shop/databases/(default)/documents/${category}`)
+      .pipe(map(data => {
+        let allItems = data['documents'].map(x => x.fields)
+        let neededItem: ItemRating = allItems.find(x=>x.index.integerValue===productID);
+        let neededItemRatings = 0;
+        neededItem.reviews.arrayValue.values.forEach((el:AllRatings)=>{
+          neededItemRatings += Number(el.mapValue.fields.rating.integerValue)
+        })
+        return neededItemRatings
+      }))
   }
 }
